464add3256ea161fe72e2abd3f6fd3bffea7859c
ShamsTech005 <ssdeen313@gmail.com>
2023-12-25 01:56:46 +0100

    implement some functionality

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const ContentSecurityPolicy = require('./csp')
 const redirects = require('./redirects')
 
 const nextConfig = {
+  typscript: {
+    ignoreBuildErrors: true,
+  },
+  }
   reactStrictMode: true,
   swcMinify: true,
   images: {
