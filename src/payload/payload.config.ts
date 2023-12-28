a51450ca99f8586aa387de9b04ceda879a20cf17
ShamsTech005 <ssdeen313@gmail.com>
2023-12-28 09:45:55 +0100

    final commit

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -1,5 +1,5 @@
 import { webpackBundler } from '@payloadcms/bundler-webpack' // bundler-import
-import { mongooseAdapter } from '@payloadcms/db-mongodb' // database-adapter-import
+import { mongooseAdapter } from '@payloadcms/db-mongodb' 
 import { payloadCloud } from '@payloadcms/plugin-cloud'
 import nestedDocs from '@payloadcms/plugin-nested-docs'
 import redirects from '@payloadcms/plugin-redirects'
