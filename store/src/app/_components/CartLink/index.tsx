5d722a21a58a6466709a976d6bc9a39afee79d35
ShamsTech005 <ssdeen313@gmail.com>
2023-12-28 14:12:59 +0100

    done

---
+++ store/src/app/_components/CartLink/index.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import React, { Fragment, useEffect, useState } from 'react'
+import Link from 'next/link'
+
+import { useCart } from '../../_providers/Cart'
+
+import classes from './index.module.scss'
+import Image from 'next/image'
+
+export const CartLink: React.FC<{
+  className?: string
+}> = props => {
+  const { className } = props
+  const { cart } = useCart()
+  const [length, setLength] = useState<number>()
+
+  useEffect(() => {
+    setLength(cart?.items?.length || 0)
+  }, [cart])
+
+  return (
+    <Link className={[classes.cartLink, className].filter(Boolean).join(' ')} href="/cart">
+      <Fragment>
+        <Image src="/assets/icons/cart.svg" alt="cart" width={24} height={24} />
+        {typeof length === 'number' && length > 0 && (
+          <small className={classes.quantity}>({length})</small>
+        )}
+      </Fragment>
+    </Link>
+  )
+}
